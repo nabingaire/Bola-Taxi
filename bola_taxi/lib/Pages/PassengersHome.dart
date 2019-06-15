bf91008c5f78262ee94a936ea3664c791bfaf31e
Nabin Gaire <gairenabin2053@gmail.com>
2019-06-15 13:58:08 +0545

    Added Passengers Home page

--- bola_taxi/lib/Pages/PassengersHome.dart
+++ bola_taxi/lib/Pages/PassengersHome.dart
@@ -9,7 +9,62 @@ class _PassengersHomeState extends State<PassengersHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text("Bola Taxi"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              
+              margin: const EdgeInsets.only(bottom: 40.0),
+              currentAccountPicture: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: Image.asset(''),
+                  )
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Ride History"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Notifications"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Setting"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Logout"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
