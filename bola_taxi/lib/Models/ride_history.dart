1034b4f606eff94a873902606f1cf8b300b757c7
Roshan Chapagain <arobce@gmail.com>
2019-06-21 15:17:55 +0545

    Added Ride History Modal

---
+++ bola_taxi/lib/Models/ride_history.dart
@@ -0,0 +1,17 @@
+class RideHistory{
+  // {
+  //     "origin": "Bhaktapur",
+  //     "destination": "Kathmandu",
+  //     "rider_name": "Raja Ram",
+  //     "date": "1990/02/02",
+  //     "taxi_no": "BA14PA"
+  //   },
+
+  final int index;
+  final String destination;
+  final String riderName;
+  final String date;
+  final String taxiNo;
+
+  RideHistory({this.index,this.destination,this.riderName,this.date,this.taxiNo});
+}
