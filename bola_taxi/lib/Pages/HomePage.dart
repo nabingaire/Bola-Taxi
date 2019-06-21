c3668970a8be3fb69e345e2e2c041582e39bc09b
Nabin Gaire <gairenabin2053@gmail.com>
2019-06-21 16:04:01 +0545

    Added HomePage

---
+++ bola_taxi/lib/Pages/HomePage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              onMapCreated: (GoogleMapController controller){
+                
+              },
+            ),
+          )
+        ],
+      ),
+      
+    );
+  }
+}
