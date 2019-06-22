3b0ccde91aeb92f2d2a7ac2d4912f3f16270c48f
Roshan Chapagain <arobce@gmail.com>
2019-06-22 10:42:41 +0545

    Fixed API

--- api/request/getcompletedrequestdata.php
+++ api/request/getcompletedrequestdata.php
@@ -5,6 +5,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
     require '../config/database.php';
     require '../config/response_codes.php';
         
+
+    // var _rideHistoryData = jsonObj.toString();
+    // for (var u in _rideHistoryData) {
+    //   RideHistoryModal rideHistory = RideHistoryModal(
+    //       _rideHistoryData["index"],
+    //       _rideHistoryData["origin"],
+    //       _rideHistoryData["destination"],
+    //       _rideHistoryData["rider_name"],
+    //       _rideHistoryData["date"],
+    //       _rideHistoryData["taxi_no"]);
+    //   _rideHistoryList.add(rideHistory);
+    // }
     
     /*============ Send Data ===========*/
     $getCompletedRequestDataQuery = "SELECT * FROM completed_request";
