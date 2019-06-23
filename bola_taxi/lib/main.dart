5e2e201ddc59dc1292c824a8ae7bcb494d84eb95
Nabin Gaire <gairenabin2053@gmail.com>
2019-06-23 17:44:30 +0545

    Added ChangeState page

--- bola_taxi/lib/main.dart
+++ bola_taxi/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bola_taxi/Pages/ChangeStatus.dart';
 import 'package:bola_taxi/Pages/DriversHome.dart';
 import 'package:bola_taxi/Pages/LoginChoice.dart';
 import 'package:bola_taxi/Pages/PassengersHome.dart';
@@ -24,7 +25,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: DriversHome()
+        body: ChangeStatus()
         
       ),
     );
