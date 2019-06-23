5a5ea3e29a630c4c9379ca1eb56dc7b63138e035
Roshan Chapagain <arobce@gmail.com>
2019-06-23 12:52:35 +0545

    Added new map

--- bola_taxi/lib/Pages/PassengersHome.dart
+++ bola_taxi/lib/Pages/PassengersHome.dart
@@ -1,6 +1,7 @@
 import 'package:bola_taxi/Widgets/menu_drawer.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class PassengersHome extends StatefulWidget {
   @override
@@ -22,29 +23,83 @@ class PassengerHomeUI extends StatefulWidget {
 }
 
 class _PassengerHomeUIState extends State<PassengerHomeUI> {
-  GoogleMapController mapController;
+  LatLng _kathMandu = LatLng(27.708576, 85.3348869);
 
-  final LatLng _kathMandu = const  LatLng(27.708576,85.3348869);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  //Markers
+  List<Marker> _mapMarkers;
 
   @override
   Widget build(BuildContext context) {
+    // _mapMarkers.add(Marker(
+    //     markerId: MarkerId('ny'), position: LatLng(27.7129365, 85.3180723)));
     return Scaffold(
       appBar: AppBar(
         title: Text("Bola Taxi"),
       ),
       drawer: MenuDrawer(),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _kathMandu,
-          zoom: 12.0,
-        ),
-        myLocationEnabled: true,
+      body: Stack(
+        children: <Widget>[
+          FlutterMap(
+            options: new MapOptions(
+              center: new LatLng(27.7083355, 85.3131555),
+              zoom: 13.0,
+              onTap: _handleTap,
+            ),
+            layers: [
+              new TileLayerOptions(
+                urlTemplate: "https://api.tiles.mapbox.com/v4/"
+                    "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+                additionalOptions: {
+                  'accessToken':
+                      'pk.eyJ1IjoiYXJvYmNlIiwiYSI6ImNqeDhqZXIwbjBsczEzcnF1cjNma3VhNHcifQ.IqytwuTwvYV1ftFptax9dA',
+                  'id': 'mapbox.streets',
+                },
+              ),
+              new MarkerLayerOptions(
+                markers: [
+                  new Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: new LatLng(27.7083355, 85.3131555),
+                    builder: (ctx) => new Container(
+                          child: Icon(Icons.location_on),
+                        ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            child: Center(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 40,
+                  child: RaisedButton.icon(
+                      icon: Icon(
+                        Icons.local_taxi,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                      label: Text(
+                        'Pick me Up',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.deepPurpleAccent[400]),
+                ),
+              ),
+            ),
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          )
+        ],
       ),
     );
+  }
+
+  _handleTap(LatLng point) {
+    setState(() {
+      _mapMarkers.add(Marker(point: point));
+    });
   }
 }
