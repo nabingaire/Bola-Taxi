dcd309f65e8c79b9d896d77327380e16e368c865
Nabin Gaire <gairenabin2053@gmail.com>
2019-06-27 02:54:01 +0545

    Added Payment Page

--- bola_taxi/lib/Pages/PaymentPage.dart
+++ bola_taxi/lib/Pages/PaymentPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PaymentPage extends StatefulWidget {
   @override
@@ -23,31 +24,87 @@ class _PaymentPageState extends State<PaymentPage> {
         ),
         backgroundColor: Colors.deepPurpleAccent[400],
       ),
-      body: Center(
-          child: Container(
-        child: Column(children: <Widget>[
-          Text("Tap a payment option"),
-          Row(
-            children: <Widget>[
-              Image.asset(
-                "assets/images/khalti.png",
-                height: 100,
-                width: 100,
-              ),
-              Image.asset(
-                "assets/images/imepay.png",
-                height: 100,
-                width: 100,
-              ),
-              Image.asset(
-                "assets/images/esewa.png",
-                height: 100,
-                width: 100,
-              )
-            ],
-          ),
-        ]),
-      )),
-    ));
+      body: _PaymentPageUI(),
+        ),
+        );
+        
   }
 }
+class  _PaymentPageUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return Center(
+     child: Container(
+       child: Column(
+         children: <Widget>[
+           Text("Tap a payment option",
+           style: TextStyle(fontSize: 30,
+           color: Colors.black
+           ),
+           
+           ),
+           Container(
+            child: GestureDetector(
+             onTap: _launchURL1,
+                child: Image.asset("assets/images/esewa.png",height: 150,)
+              
+            ),
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+           ),
+           Container(
+            child: GestureDetector(
+             onTap: _launchURL2,
+                child: Image.asset("assets/images/khalti.png",height: 150,)
+              
+            ),
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+           ),
+          Container(
+            child: GestureDetector(
+             onTap: _launchURL3,
+                child: Image.asset("assets/images/imepay.png",height: 150,)
+              
+            ),
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+           )
+
+
+         ],
+       ),
+
+     ) 
+   );
+
+
+
+  }
+}
+_launchURL1() async{
+ const url1 = 'https://esewa.com.np/#/home';
+  if (await canLaunch(url1)) {
+      await launch(url1);
+    } else {
+      throw 'Could not launch $url1';
+    }
+   
+}
+_launchURL2() async{
+  const url2 = 'https://khalti.com/';
+   if (await canLaunch(url2)) {
+      await launch(url2);
+    } else {
+      throw 'Could not launch $url2';
+    }
+   
+}
+_launchURL3() async{
+  const url3 = 'http://www.imepay.com.np/';
+   if (await canLaunch(url3)) {
+      await launch(url3);
+    } else {
+      throw 'Could not launch $url3';
+    }
+   
+}
+
+
