b1118e26c3185bea1e299d8078c9504e91417ca7
Nabin Gaire <gairenabin2053@gmail.com>
2019-06-27 01:18:14 +0545

    Chnaged main dart

--- bola_taxi/lib/main.dart
+++ bola_taxi/lib/main.dart
@@ -37,7 +37,7 @@ class _HomePageState extends State<HomePage> {
 
     return MaterialApp(
       home: Scaffold(
-        body: SignUpChoice(),        
+        body: LoginChoice(),        
       ),
     );
   }
