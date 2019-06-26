ce65503882bd4fd8da93816837d6e901480b48cf
Nabin Gaire <gairenabin2053@gmail.com>
2019-06-27 01:00:22 +0545

    Changed something

--- bola_taxi/lib/main.dart
+++ bola_taxi/lib/main.dart
@@ -37,7 +37,7 @@ class _HomePageState extends State<HomePage> {
 
     return MaterialApp(
       home: Scaffold(
-        body: LoginChoice(),        
+        body: SignUpChoice(),        
       ),
     );
   }
