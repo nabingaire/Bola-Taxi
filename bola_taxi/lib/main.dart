51c1e059cbd9185a0ad9f5db2d841ac5cd91b807
Roshan Chapagain <arobce@gmail.com>
2019-06-27 04:48:11 +0545

    Fixed errors

--- bola_taxi/lib/main.dart
+++ bola_taxi/lib/main.dart
@@ -38,7 +38,7 @@ class _HomePageState extends State<HomePage> {
 
     return MaterialApp(
       home: Scaffold(
-        body: Splash(),        
+        body: LoginChoice(),        
       ),
     );
   }
